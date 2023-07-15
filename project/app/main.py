7d63ea4575498b51792c05c2e5113a35e6ed85b7
Fabio Rocha <gomesrocha@gmail.com>
2023-07-15 20:23:19 -0300

    insert sentry support

--- project/app/main.py
+++ project/app/main.py
@@ -2,7 +2,17 @@ import os
 import logging
 
 from fastapi import FastAPI
+import sentry_sdk
 
+
+sentry_sdk.init(
+    dsn="https://5c74c0bf64424183a3d8fea7a803a9b0@o4505535984828416.ingest.sentry.io/4505535986335744",
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
+)
 
 from api import text_extract  # updated
 
