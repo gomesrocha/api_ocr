92fe05149c447a6a8e6e0a85430367834042848e
Fabio Rocha <gomesrocha@gmail.com>
2023-07-15 20:54:57 -0300

    Update

--- project/app/main.py
+++ project/app/main.py
@@ -4,7 +4,7 @@ import logging
 from fastapi import FastAPI
 import sentry_sdk
 
-
+from api import text_extract  # updated
 sentry_sdk.init(
     dsn="https://5c74c0bf64424183a3d8fea7a803a9b0@o4505535984828416.ingest.sentry.io/4505535986335744",
 
@@ -14,10 +14,7 @@ sentry_sdk.init(
     traces_sample_rate=1.0,
 )
 
-from api import text_extract  # updated
-
 log = logging.getLogger("uvicorn")
-
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(text_extract.router)
