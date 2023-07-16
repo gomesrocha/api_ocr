87678579aa26aa5be02b997605b04ae2bbc84140
Fabio Rocha <gomesrocha@gmail.com>
2023-07-16 16:40:27 -0300

    <fix> Technical debt correction
    Correction of variable names of the extract_text function

--- project/app/api/text_extract.py
+++ project/app/api/text_extract.py
@@ -9,11 +9,11 @@ from app.model.TextSchema import TextExtractDocument as ted
 router = APIRouter()
 
 @router.post("/extract_text")
-async def extract_text(Images: List[UploadFile] = File(...)) -> ted:
+async def extract_text(Images_in: List[UploadFile] = File(...)) -> ted:
     response = {}
     s = time.time()
     image_name = ""
-    for img in Images:
+    for img in Images_in:
         print("Images Uploaded: ", img.filename)
         temp_file = fileUpload._save_file_to_server(img, path="./images/", save_as=img.filename)
         text = await ocr.read_image(temp_file)
@@ -21,5 +21,5 @@ async def extract_text(Images: List[UploadFile] = File(...)) -> ted:
         image_name = img.filename
         os.remove(temp_file)
     response["Time Taken"] = round((time.time() - s),2)
-    outInf = ted(file_name=image_name, text = text, time_taken = response["Time Taken"])
-    return outInf
+    image_out = ted(file_name=image_name, text = text, time_taken = response["Time Taken"])
+    return image_out
